f7472b443563c4ea45a1e1f5fda20e1769a0c071
jwryu87 <jwryu87@gmail.com>
2020-07-09 12:40:54 +0900

    Initial commit

---
+++ spotify_api_1.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+#
+
+from selenium import webdriver
+from urllib.request import urlopen
+import urllib
+import pandas as pd
+import json
+import sqlite3
+import webbrowser
+
+if __name__ == "__main__":
+    
---
+++ test.py
@@ -0,0 +1,59 @@
+import sys
+import requests
+import base64
+import json
+import logging
+
+client_id = "74cbd487458843f1ad3f5fa1e914c02f"
+client_secret = "752e4ed11062473f9da9076c4499d51b"
+
+
+def main():
+
+    headers = get_headers(client_id, client_secret)
+
+    ## Spotify Search API
+    params = {
+        "q": "BTS",
+        "type": "artist",
+        "limit": "5"
+    }
+
+
+    r = requests.get("https://api.spotify.com/v1/search", params=params, headers=headers)
+
+    print(r.status_code)
+    print(r.text)
+    print(r.headers)
+    sys.exit(0)
+
+
+
+def get_headers(client_id, client_secret):
+
+    endpoint = "https://accounts.spotify.com/api/token"
+    encoded = base64.b64encode("{}:{}".format(client_id, client_secret).encode('utf-8')).decode('ascii')
+
+    headers = {
+        "Authorization": "Basic {}".format(encoded)
+    }
+
+    payload = {
+        "grant_type": "client_credentials"
+    }
+
+    r = requests.post(endpoint, data=payload, headers=headers)
+
+    access_token = json.loads(r.text)['access_token']
+
+    headers = {
+        "Authorization": "Bearer {}".format(access_token)
+    }
+
+    return headers
+
+
+
+
+if __name__=='__main__':
+    main()

214d84dce8e3ba7debc84323c8e3f5ba00088dcb
jwryu87 <jwryu87@gmail.com>
2020-07-29 09:14:26 +0900

    commit-20200714

--- test.py
+++ test.py
@@ -29,10 +29,10 @@ def main():
     print(cursor.fetchall())
 
     print("success")
-
-    query = "INSERT INTO artist_genres (artist_id, genre, updated_at) VALUES ('{1}', '{0}', NOW())".format('1234', 'hip-hop')
-    cursor.execute(query)
-    conn.commit()
+    #
+    # query = "INSERT INTO artist_genres (artist_id, genre, updated_at) VALUES ('{1}', '{0}', NOW())".format('1234', 'hip-hop')
+    # cursor.execute(query)
+    # conn.commit()
 
 
     sys.exit(0)
@@ -43,97 +43,16 @@ def main():
     params = {
         "q": "BTS",
         "type": "artist",
-        "limit": "5"
+        "limit": "1"
     }
 
     r = requests.get("https://api.spotify.com/v1/search", params=params, headers=headers)
-
-    print(r.text)
-    print(type(r.text))
-    sys.exit(0)
-
-    try:
-        r = requests.get("https://api.spotify.com/v1/search", params=params, headers=headers)
-
-    except:
-        logging.error(r.text)
-        sys.exit(1)
-
-
-    r = requests.get("https://api.spotify.com/v1/search", params=params, headers=headers)
-
-    if r.status_code != 200:
-        logging.error(r.text)
-
-        if r.status_code == 429:
-
-            retry_after = json.loads(r.headers)['Retry-After']
-            time.sleep(int(retry_after))
-
-            r = requests.get("https://api.spotify.com/v1/search", params=params, headers=headers)
-
-        ## access_token expired
-        elif r.status_code == 401:
-
-            headers = get_headers(client_id, client_secret)
-            r = requests.get("https://api.spotify.com/v1/search", params=params, headers=headers)
-
-        else:
-            sys.exit(1)
-
-
-    # Get BTS' Albums
-
-    r = requests.get("https://api.spotify.com/v1/artists/3Nrfpe0tUJi4K4DXYWgMUX/albums", headers=headers)
 
     raw = json.loads(r.text)
-
-    total = raw['total']
-    offset = raw['offset']
-    limit = raw['limit']
-    next = raw['next']
-
-    albums = []
-    albums.extend(raw['items'])
-
-    ## 난 100개만 뽑아 오겠다
-    while next:
-
-        r = requests.get(raw['next'], headers=headers)
-        raw = json.loads(r.text)
-        next = raw['next']
-        print(next)
-
-        albums.extend(raw['items'])
-        count = len(albums)
-
-    print(len(albums))
+    print(raw['artists'])
 
 
-
-def get_headers(client_id, client_secret):
-
-    endpoint = "https://accounts.spotify.com/api/token"
-    encoded = base64.b64encode("{}:{}".format(client_id, client_secret).encode('utf-8')).decode('ascii')
-
-    headers = {
-        "Authorization": "Basic {}".format(encoded)
-    }
-
-    payload = {
-        "grant_type": "client_credentials"
-    }
-
-    r = requests.post(endpoint, data=payload, headers=headers)
-
-    access_token = json.loads(r.text)['access_token']
-
-    headers = {
-        "Authorization": "Bearer {}".format(access_token)
-    }
-
-    return headers
-
+    sys.exit(0)
 
 
 

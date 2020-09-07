3f658aae06e0b871f6f1cb55034edd8bd9072333
jwryu87 <jwryu87@gmail.com>
2020-09-07 16:01:24 +0900

    Title: Data Lake

--- Data Lake/spotify_14.2.py
+++ Data Lake/spotify_14.2.py
@@ -19,9 +19,12 @@ username = "jwryu87"
 database = "production"
 password = "Tkekfl!38"
 
+# top_tracks과 audio_features 를 다 가져와서
+# parquet 화 로 변형을 시키고
+# S3 의 파티션화된 폴더 안에 데이터를 저장
 
 def main():
-
+    # RDS 접속
     try:
         conn = pymysql.connect(host, user=username, passwd=password, db=database, port=port, use_unicode=True, charset='utf8')
         cursor = conn.cursor()
@@ -30,7 +33,7 @@ def main():
         sys.exit(1)
 
     headers = get_headers(client_id, client_secret)
-
+########################################################################################################################
     # RDS - 아티스트 ID를 가져오고
     cursor.execute("SELECT id FROM artists LIMIT 10")
 
@@ -40,18 +43,19 @@ def main():
         "popularity": "popularity",
         "external_url": "external_urls.spotify"
     }
-    # Top Tracks Spotify 가져오고
+########################################################################################################################
+    # API - Top Tracks Spotify 가져오고
     top_tracks = []
     for (id, ) in cursor.fetchall():
 
-        URL = "https://api.spotify.com/v1/artists/{}/top-tracks".format(id)
+        URL = "https://api.spotify.com/v1/artists/{}/top-tracks".format(id) # loop를 돌면서 artists ID 가 들어간 URL을 만든다
         params = {
             'country': 'US'
         }
-        r = requests.get(URL, params=params, headers=headers)
-        raw = json.loads(r.text)
+        r = requests.get(URL, params=params, headers=headers) # API 요청 (params, headers 가 필요함) (headers 는 위에서 define을 했음)
+        raw = json.loads(r.text) # 가져온 API json 형식의 데이터를 raw에 저장
 
-        for i in raw['tracks']:
+        for i in raw['tracks']: # 필요한 것은 raw 의 tracks
             top_track = {}
             for k, v in top_track_keys.items():
                 top_track.update({k: jsonpath.jsonpath(i, v)})
@@ -61,8 +65,18 @@ def main():
     # track_ids
     track_ids = [i['id'][0] for i in top_tracks]
 
+    print(track_ids)
+
+    # (parquet 화 시 struct 에 문제가 있을 수 있다.)
+    # parquet 화 과정에서 아래와 유사한 데이터 파이프라인 과정이 필요
+    # > s3의 가장 raw 데이터가 들어옴
+    # > parquet 화 하고 싶은 몇개의 데이터만 뽑아옴
+    # > 이것들만 parquet 화 하여 다시 다른 S3 버킷안에 저장
+
     top_tracks = pd.DataFrame(top_tracks)
-    top_tracks.to_parquet('top-tracks.parquet', engine='pyarrow', compression='snappy')
+    top_tracks.to_parquet('top-tracks.parquet', engine='pyarrow', compression='snappy') # parquet 화 된것이 더 퍼포먼스가 좋고 압축할떄 더 효율이 있다.
+
+    sys.exit()
 
     dt = datetime.utcnow().strftime("%Y-%m-%d")
 

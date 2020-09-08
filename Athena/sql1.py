b8045fd23966b07ae29aa846c113b307adac923e
jwryu87 <jwryu87@gmail.com>
2020-09-08 17:13:01 +0900

    Title: Athena

--- Athena/sql1.py
+++ Athena/sql1.py
@@ -14,12 +14,18 @@ MSCK REPAIR table top_tracks
 
 create external table if not exists audio_features(
   id string,
-  artists_id string,
-  name string,
-  popularity int,
-  external_url string
+  danceability double,
+  energy double,
+  key int,
+  loudness double,
+  mode int,
+  speechiness double,
+  acousticness double,
+  instrumentalness double,
 ) partitioned by (dt string)
-stored as parquet location 's3://spotify--artists-rjw/audio-features/' tblproperties("parquet.compress"="SNAPPY")
+stored as parquet location 's3://spotify-artists-rjw/audio_features/' tblproperties("parquet.compress"="SNAPPY")
+
+
 
 
 MSCK REPAIR table audio_features
